d02e2bb84ef42132da7a04fd6ae6fd9b49fc437c
chrisanthyago <chrisanthyago.developer@gmail.com>
2026-08-08 10:53:43 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,174 @@
+export default {
+    // Fetch event handler to serve static assets
+    async fetch(request, env) {
+        const url = new URL(request.url);
+
+        // Redirect root path to /tv-online
+        if (url.pathname === '/') {
+            url.pathname = '/tv-online';
+            return Response.redirect(url.toString(), 301);
+        }
+
+        let response = await env.ASSETS.fetch(request);
+
+        // If the response is HTML, set the Content-Security-Policy header based on the origins stored in the KV store
+        const contentType = response.headers.get('Content-Type') || '';
+        if (contentType.includes('text/html')) {
+            try {
+                const kvKeyCSP = env.CLOUDFLARE_KV_KEY_CSP;
+
+                let origins = await env.KEY_VALUE.get(kvKeyCSP);
+
+                if (!origins) {
+                    try {
+                        // Fetch the config.json file from the ASSETS special binding
+                        const configResponse = await env.ASSETS.fetch(new URL('/assets/config.json', request.url));
+                        if (!configResponse.ok) {
+                            throw new Error(`Failed to fetch config.json: ${configResponse.status} ${configResponse.statusText}`);
+                        }
+                        const configData = await configResponse.json();
+
+                        // Extract unique origins from the config.json file
+                        const uniqueOrigins = new Set();
+                        configData.webpages?.forEach(webpage => {
+                            webpage.urls.forEach(url => uniqueOrigins.add(new URL(url).origin));
+                        });
+                        origins = Array.from(uniqueOrigins).join(' ');
+
+                        // Update the KV store with the unique origins
+                        await env.KEY_VALUE.put(kvKeyCSP, origins);
+                        console.log(`KV key ${kvKeyCSP} updated successfully`);
+
+                    } catch (error) {
+                        error.message = `Failed to update KV key ${kvKeyCSP}: ${error.message}`;
+                        throw error;
+                    }
+                }
+
+                // Set the Content-Security-Policy header with the origins from the KV store
+                const cspValue = [
+                    "default-src 'self'",
+                    "script-src 'self'",
+                    "style-src 'self' https://www.w3schools.com",
+                    "img-src 'self' data:",
+                    "object-src 'none'",
+                    `frame-src 'self' ${origins}`,
+                    `connect-src 'self' ${origins}`
+                ].join('; ');
+                response = new Response(response.body, response);
+                response.headers.set('Content-Security-Policy', cspValue);
+
+            } catch (error) {
+                error.message = `Error setting Content-Security-Policy header: ${error.message}`;
+                console.error(error);
+            }
+        }
+
+        return response;
+    },
+
+    // Scheduled event handler to trigger the Cloud Run endpoint based on the schedule defined in config.json and KV store
+    async scheduled(controller, env, ctx) {
+        ctx.waitUntil(
+            (async () => {
+                try {
+                    const kvKeyCron = env.CLOUDFLARE_KV_KEY_CRON;
+
+                    let times = await env.KEY_VALUE.get(kvKeyCron);
+
+                    if (!times) {
+                        try {
+                            // Fetch the config.json file from the ASSETS special binding
+                            const configResponse = await env.ASSETS.fetch(new URL('/assets/config.json', 'http://internal-assets'));
+                            if (!configResponse.ok) {
+                                throw new Error(`Failed to fetch config.json: ${configResponse.status} ${configResponse.statusText}`);
+                            }
+                            const configData = await configResponse.json();
+
+                            // Extract unique times from the config.json file
+                            const uniqueTimes = new Set();
+                            configData.webpages?.forEach(webpage => {
+                                if (webpage.isTime24H) {
+                                    uniqueTimes.add(webpage.time);
+                                }
+                            });
+                            times = Array.from(uniqueTimes).sort().join(' ');
+
+                            // Update the KV store with the unique times
+                            await env.KEY_VALUE.put(kvKeyCron, times);
+                            console.log(`KV key ${kvKeyCron} updated successfully`);
+
+                        } catch (error) {
+                            error.message = `Failed to update KV key ${kvKeyCron}: ${error.message}`;
+                            throw error;
+                        }
+                    }
+
+                    const scheduledTime = new Date(controller.scheduledTime);
+                    scheduledTime.setUTCMinutes(scheduledTime.getUTCMinutes() + parseInt(env.CRONS_DELAY, 10));
+                    const targetTime = scheduledTime.toISOString().slice(11, 16);
+
+                    const scheduledCrons = env.SCHEDULED_CRONS.split(' ');
+                    const scheduledTimes = times.split(' ');
+                    const scheduledTasks = [...new Set([...scheduledCrons, ...scheduledTimes])].sort();
+
+                    const cronsInterval = parseInt(env.CRONS_INTERVAL, 10);
+
+                    // Check if the target time is within the schedule defined in scheduledCrons and config.json
+                    if (!evaluateSchedule(targetTime, scheduledTasks, cronsInterval)) {
+                        throw new Error(`Target time ${targetTime} is outside the schedule defined in the scheduled tasks. No run performed.`);
+                    }
+
+                    const cloudRunEndpoint = env.CLOUD_RUN_ENDPOINT;
+                    const cloudRunApiToken = env.CLOUD_RUN_API_TOKEN;
+
+                    // Trigger the Cloud Run endpoint with the Target Time and Crons Interval
+                    const cloudRunResponse = await fetch(cloudRunEndpoint, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${cloudRunApiToken}`
+                        },
+                        body: JSON.stringify({
+                            'x-target-time-utc': targetTime,
+                            'x-crons-interval': cronsInterval,
+                            'x-origin': 'Cloudflare Pages Cron Trigger',
+                            'x-message': 'Scheduled task triggered'
+                        })
+                    });
+
+                    if (!cloudRunResponse.ok) {
+                        throw new Error(`Failed to trigger Cloud Run endpoint: ${cloudRunResponse.status} ${cloudRunResponse.statusText}`);
+                    }
+
+                    const cloudRunData = await cloudRunResponse.json();
+                    console.log('Successfully triggered Cloud Run endpoint:');
+                    console.log(cloudRunData);
+
+                } catch (error) {
+                    error.message = `Error triggering Cloud Run endpoint: ${error.message}`;
+                    console.error(error);
+                }
+            })()
+        );
+    }
+};
+
+// Helper function to evaluate if the target time is within the schedule defined in scheduledCrons and config.json
+function evaluateSchedule(targetTime, scheduledTasks, cronsInterval) {
+    // Convert time in HH:mm format to total minutes for easier comparison
+    const getMinutes = (time) => {
+        const [hours, minutes] = time.split(':').map(Number);
+        return 60 * hours + minutes;
+    };
+
+    const minutesTarget = getMinutes(targetTime);
+    for (const scheduledTask of scheduledTasks) {
+        const minutesScheduled = getMinutes(scheduledTask);
+        const difference = minutesScheduled - minutesTarget;
+        if ((difference >= 0 && difference < cronsInterval) || (difference < 0 && difference < cronsInterval - 1440)) {
+            return true;
+        }
+    };
+    return false;
+}
---
+++ src/scripts/update-kv.mjs
@@ -0,0 +1,75 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const accountId = process.env.CLOUDFLARE_ACCOUNT_ID;
+const kvNamespaceId = process.env.CLOUDFLARE_KV_NAMESPACE_ID;
+const kvKeyCron = process.env.CLOUDFLARE_KV_KEY_CRON;
+const kvKeyCSP = process.env.CLOUDFLARE_KV_KEY_CSP;
+// Start Date - June 19, 2026
+// End Date - September 18, 2026
+const apiToken = process.env.CLOUDFLARE_API_TOKEN;
+
+(async () => {
+    if (!accountId || !kvNamespaceId || !kvKeyCron || !kvKeyCSP || !apiToken) {
+        console.error('Missing required environment variables.');
+        process.exit(1);
+    }
+
+    try {
+        // Read the config.json file
+        const configPath = path.resolve('/public/assets/config.json');
+        if (!fs.existsSync(configPath)) {
+            throw new Error(`Config file not found at path: ${configPath}`);
+        }
+        const configContent = fs.readFileSync(configPath, 'utf-8');
+        const configData = JSON.parse(configContent);
+
+        // Extract unique times and origins from the config data
+        const uniqueTimes = new Set();
+        const uniqueOrigins = new Set();
+        configData.webpages?.forEach(webpage => {
+            if (webpage.isTime24H) {
+                uniqueTimes.add(webpage.time);
+            }
+            webpage.urls.forEach(url => uniqueOrigins.add(new URL(url).origin));
+        });
+
+        const times = Array.from(uniqueTimes).sort().join(' ');
+        const origins = Array.from(uniqueOrigins).join(' ');
+
+        // Prepare the request to update the KV store
+        const kvUrl = `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${kvNamespaceId}/values`;
+        const kvInit = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${apiToken}`,
+                'Content-Type': 'text/plain'
+            }
+        };
+
+        // Update the KV key for Cron times
+        const responseCron = await fetch(`${kvUrl}/${kvKeyCron}`, {
+            ...kvInit,
+            body: times
+        });
+        if (!responseCron.ok) {
+            throw new Error(`Failed to update KV key ${kvKeyCron}: ${responseCron.status} ${responseCron.statusText}`);
+        }
+        console.log(`KV key ${kvKeyCron} updated successfully with:`, times);
+
+        // Update the KV key for CSP origins
+        const responseCSP = await fetch(`${kvUrl}/${kvKeyCSP}`, {
+            ...kvInit,
+            body: origins
+        });
+        if (!responseCSP.ok) {
+            throw new Error(`Failed to update KV key ${kvKeyCSP}: ${responseCSP.status} ${responseCSP.statusText}`);
+        }
+        console.log(`KV key ${kvKeyCSP} updated successfully with:`, origins);
+
+    } catch (error) {
+        error.message = `Error updating KV store: ${error.message}`;
+        console.error(error);
+        process.exit(1);
+    }
+})();
